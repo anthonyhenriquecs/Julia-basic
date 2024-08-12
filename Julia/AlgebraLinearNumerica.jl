dfbb0a84d77f8748ca2d40064b1be8252b18f984
Henrique Anthony <107704669+anthonyhenriquecs@users.noreply.github.com>
2024-08-12 20:39:44 -0300

    Add files via upload

---
+++ Julia/AlgebraLinearNumerica.jl
@@ -0,0 +1,3 @@
+v = [1.1;2.2;3.3] #Vetor
+v + v #da pra se usar operações de soma, mult, div, sub em vetoes, de vetor com vetor
+M = [5  4  -2; 1 2 3] #Criar matriz, os numeros por colunas se divide com espaço, e por linhas com ;
---
+++ Julia/Documentacao.jl
@@ -0,0 +1,31 @@
+#Para escrever um texto usamos #
+#= para comentarios de multiplas
+linhas=#
+
+#Matematica basica 
+1+1
+2-1
+2*1
+4/2
+2^3#Potencia
+5%2#Modulo
+
+#PEMDAS
+#=
+Potencia
+Exponencial
+Multiplicação
+Divisão
+Adição
+Subtração
+=#
+
+#Operadores de comparação são booleanos, que dizem se algo é verdadeiro ou falso
+
+3 > 2 # 3 é Maior que 2?
+3 < 2 # 3 é menor que 2?
+
+#Operadores de comparação | ou; & e 
+#Operadores de curto circuito || ou; && e;   ELE É CHAMADO ASSIM, pois começa avaliando o lado esquerdo
+#True == 1
+#False == 0
